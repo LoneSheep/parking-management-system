5e1d890ff111c59d3b3f4a4756a57943dd69e3a1
LoneSheep <yuarisarham@gmail.com>
2023-07-11 09:30:22 +0700

    change db connection

--- users/includes/dbconnection.php
+++ users/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "vpmsdb");
+$con=mysqli_connect("localhost", "root", "db_ya", "vpmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Failed".mysqli_connect_error();
 }
