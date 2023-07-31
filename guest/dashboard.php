e26465991c97602544433de2b450c5207c8669e8
LoneSheep <yuarisarham@gmail.com>
2023-07-31 16:47:49 +0700

    fix qrcode

--- guest/dashboard.php
+++ guest/dashboard.php
@@ -169,92 +169,88 @@ if (isset($_COOKIE['guestId'])) {
         <div class="clearfix"></div>
         
         <?php
+    if (isset($guestId)) {
 
-        if (isset($guestId)) {
+        include "../phpqrcode/qrlib.php"; 
 
-            include "../phpqrcode/qrlib.php"; 
-            $path = "qr_saves/";
+        // Generate a random token
+        $token = bin2hex(random_bytes(16)); // 16 bytes = 128 bits
 
-            // Generate a random token
-            $token = bin2hex(random_bytes(16)); // 16 bytes = 128 bits
+        // Fetch user's qrimage from the database
+        $sql = "SELECT qrimage FROM tblguest WHERE ID = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bind_param("i", $guestId);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-            // Fetch user's qrimage from the database
-            $sql = "SELECT qrimage FROM tblguest WHERE ID = ?";
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $qrImage = $user['qrimage'];
+        } else {
+            $qrImage = null; // Or any default value you want
+        }
+        $stmt->close();
+
+        // If the user doesn't already have a QR image
+        if (empty($qrImage)) {
+            $text = $token; 
+            $filename = uniqid().".png";
+            $ecc = 'H';
+            $pixel_Size = 7;
+            $frame_Size = 7;
+
+            // Generate QR code image data into a variable
+            ob_start();
+            QRcode::png($text, false, $ecc, $pixel_Size, $frame_Size);  // False as second argument to output to buffer instead of file
+            $imageData = ob_get_clean();
+
+            // Authenticate with Google Cloud
+            $storage = new StorageClient([
+                'projectId' => '../my-project-388313',
+                'keyFilePath' => '../my-project-388313-8d498336248d.json'
+            ]);
+
+            // The name of the bucket you're using
+            $bucketName = 'parkingsystem2023';
+
+            // Upload the file to the bucket
+            $bucket = $storage->bucket($bucketName);
+            $bucket->upload(
+                $imageData,  // Use $imageData instead of file
+                [
+                    'name' => $filename,  // Name the object with the $filename
+                    'metadata' => [
+                        'contentType' => 'image/png',  // Set content type so GCS knows it's an image
+                    ],
+                ]
+            );
+
+            // Generate a public URL for the object
+            $qrImage = sprintf('https://storage.googleapis.com/%s/%s', $bucketName, $filename);
+
+            // prepare sql statement
+            $sql = "UPDATE tblguest SET qrimage = ?, token = ? WHERE ID = ?";
+
+            // create a prepared statement
             $stmt = $con->prepare($sql);
-            $stmt->bind_param("i", $guestId);
+
+            // bind parameters
+            $stmt->bind_param("ssi", $qrImage, $token, $guestId);
+
+            // execute the query
             $stmt->execute();
-            $result = $stmt->get_result();
 
-            if ($result->num_rows > 0) {
-                $user = $result->fetch_assoc();
-                $qrImage = $user['qrimage'];
-            } else {
-                $qrImage = null; // Or any default value you want
-            }
+            // close the statement
             $stmt->close();
-
-
-
-
-            // If the user doesn't already have a QR image
-            if (empty($qrImage)) {
-                $text = $token; 
-                $filename = uniqid().".png";
-                $file = $path.$filename;
-                $ecc = 'H';
-                $pixel_Size = 10;
-                $frame_Size = 10;
-
-                if (!file_exists($path)) {
-                    mkdir($path);
-                }
-
-                QRcode::png($text, $file, $ecc, $pixel_Size, $frame_Size);
-
-                // Authenticate with Google Cloud
-                $storage = new StorageClient([
-                    'projectId' => 'my-project-388313',
-                    'keyFilePath' => 'my-project-388313-8d498336248d.json'
-                ]);
-
-                // The name of the bucket you're using
-                $bucketName = 'parkingsystem2023';
-
-                // Upload the file to the bucket
-                $bucket = $storage->bucket($bucketName);
-                $bucket->upload(
-                    fopen($file, 'r')
-                );
-
-                // Delete the file from local system
-                unlink($file);
-
-                // Generate a public URL for the object
-                $qrImage = sprintf('https://storage.googleapis.com/%s/%s', $bucketName, $filename);
-
-                // prepare sql statement
-                $sql = "UPDATE tblguest SET qrimage = ?, token = ? WHERE ID = ?";
-
-                // create a prepared statement
-                $stmt = $con->prepare($sql);
-
-                // bind parameters
-                $stmt->bind_param("ssi", $qrImage, $token, $guestId);
-
-                // execute the query
-                $stmt->execute();
-
-                // close the statement
-                $stmt->close();
-            }
-
-            // close the connection
-            $con->close();
-
-            echo "<center><img src='".$qrImage."'></center>";
         }
 
-        ?>
+        // close the connection
+        $con->close();
+
+        echo "<center><img src='".$qrImage."'></center>";
+    }
+    ?>
+
 
         <!-- Footer -->
         <?php include_once('includes/footer.php');?>
