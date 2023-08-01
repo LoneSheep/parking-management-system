e6df1b39368c9ffb98cb733654dfa8480625a226
LoneSheep <yuarisarham@gmail.com>
2023-08-01 23:08:37 +0700

    update tblpayments

--- users/status-updater.php
+++ users/status-updater.php
@@ -1,8 +1,6 @@
 <?php
 // connect to database
 include('../dbconnection.php');
-
-// Include the Cloud Storage library.
 require '../vendor/autoload.php';
 use Google\Cloud\Storage\StorageClient;
 
@@ -30,12 +28,17 @@ $result = $con->query($sql);
 while($row = $result->fetch_assoc()) {
     $qrimage = $row['qrimage'];
 
-    // remove the Cloud Storage URL prefix to get object name
-    $objectName = str_replace('https://storage.googleapis.com/'.$bucketName.'/', '', $qrimage);
+    // If qrimage is not NULL
+    if ($qrimage != NULL) {
+        // remove the Cloud Storage URL prefix to get object name
+        $objectName = str_replace('https://storage.googleapis.com/'.$bucketName.'/', '', $qrimage);
 
-    // Delete the object from Cloud Storage
-    $object = $bucket->object($objectName);
-    $object->delete();
+        // Delete the object from Cloud Storage
+        $object = $bucket->object($objectName);
+        if ($object->exists()) {
+            $object->delete();
+        }
+    }
 
     // prepare sql statement to set qrimage value to NULL for this user
     $sql = "UPDATE tblregusers SET qrimage = NULL WHERE ID = ?";
@@ -48,6 +51,18 @@ while($row = $result->fetch_assoc()) {
 
     // execute the query
     $stmt->execute();
+
+    // prepare sql statement to update payment_status value to PENDING for this user in tblpayments
+    $sql_payments = "UPDATE tblpayments SET payment_status = 'PENDING' WHERE user_id = ?";
+
+    // create a prepared statement
+    $stmt_payments = $con->prepare($sql_payments);
+
+    // bind parameters
+    $stmt_payments->bind_param("i", $row['ID']);
+
+    // execute the query
+    $stmt_payments->execute();
 }
 
 // close the statement
@@ -56,6 +71,6 @@ $stmt->close();
 // close the connection
 $con->close();
 
-echo "QR codes deleted and qrimage values removed successfully.";
+echo "QR codes deleted, qrimage values removed and payment_status updated successfully.";
 
 ?>
