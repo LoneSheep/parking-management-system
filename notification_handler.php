a947b89eea0973a192077ed8bd52a48743782624
LoneSheep <yuarisarham@gmail.com>
2023-08-02 19:05:40 +0700

    fix orderID

--- notification_handler.php
+++ notification_handler.php
@@ -14,39 +14,25 @@ $order_id = $notif->order_id;
 error_log("Order ID = $order_id, Transaction status = $transaction, Fraud status = $fraud");
 
 // You need to update transaction data in the database
-if ($transaction == 'capture') {
-    if ($fraud == 'challenge') {
-        // TODO: Log this for manual verification later in your system
-    } else if ($fraud == 'accept') {
-        // Payment is successful, you can update the payment status
-    }
-} else if ($transaction == 'settlement') {
+if ($transaction == 'settlement') {
     // Update payment status in your database to 'DONE'
-    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'DONE' WHERE order_id = ?");
+    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'DONE' WHERE orderID = ?");
     $stmt->bind_param("s", $order_id);
     $stmt->execute();
-
-} else if ($transaction == 'pending') {
-    // The customer has not yet completed the payment. No need to do anything.
 } else if ($transaction == 'deny') {
     // Payment is denied, update payment_status and status in your database.
-    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'DENY' WHERE order_id = ?");
+    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'DENY' WHERE orderID = ?");
     $stmt->bind_param("s", $order_id);
     $stmt->execute();
-
-    
 } else if ($transaction == 'expire') {
     // Payment has expired, update payment_status and status in your database.
-    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'EXPIRE' WHERE order_id = ?");
+    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'EXPIRE' WHERE orderID = ?");
     $stmt->bind_param("s", $order_id);
     $stmt->execute();
-
-    
 } else if ($transaction == 'cancel') {
     // Payment is cancelled, update payment_status and status in your database.
-    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'CANCEL' WHERE order_id = ?");
+    $stmt = $con->prepare("UPDATE tblpayments SET payment_status = 'CANCEL' WHERE orderID = ?");
     $stmt->bind_param("s", $order_id);
     $stmt->execute();
-
 }
 ?>
